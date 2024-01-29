90a9c0e8afa53527dcc0bc313d1d1df9d4c0187b
Slyxes5 <avantakun@gmail.com>
2024-01-29 15:50:35 +0800

    donee

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const members = require('./members');
 const users = require('./users')
 const http = require('http')
-const currentDate = new Date().toISOString();
+// const currentDate = new Date().toISOString();
+const moment = require('moment')
 
 const server = http.createServer((req, res) =>{
     res.statusCode=200
     res.setHeader('Content-Type', 'text/json')
     const url = req.url
     if(url ==='/'){
-        res.write("This is the home page")
+        res.write("This is the home page \n")
+        res.write(moment().format('MMMM Do YYYY, h:mm:ss a'))
     }
     if(url ==='/about'){
         res.write(JSON.stringify({
             'status' : 'succes', 
             'message' : 'response success', 
-            'description' : 'Exercise #3',
-            'date' : currentDate,
-            'data' : members
+            'description' : 'Exercise #3'  ,
+            'mebers' : members
         }))
+        
     }
     if(url==='/users'){
+        res.write(JSON.stringify({
+            'users' : users, 
+        }))
     }else{
-        res.write('...')
     }
-res.end()
-}).listen(3000)
+res.end();
+})
+
+const port = 3000;
+const hostname = '127.0.0.1';
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
--- members.js
+++ members.js
@@ -1,15 +1,42 @@
 const members = [
     {
       id: 1,
-      nim: "123456",
-      nama: "John Doe",
-      nomorTelp: "081234567890"
+      nim: "654321",
+      nama: "Politon, Vrandly Theodore",
+      nomorTelp: "+62"
     },
     {
         id: 2,
         nim: "654321",
-        nama: "SUMSUM Doe",
-        nomorTelp: "08256578"
+        nama: "Joshua Tengker",
+        nomorTelp: "+62"
+      },
+      {
+        id: 3,
+        nim: "654321",
+        nama: "Lukas, Jonathan",
+        nomorTelp: "+62"
+      },
+
+      {
+        id: 4,
+        nim: "654321",
+        nama: "Joshua Tengker",
+        nomorTelp: "+62"
+      },
+
+      {
+        id: 2,
+        nim: "654321",
+        nama: "Timothy",
+        nomorTelp: "+62"
+      },
+
+      {
+        id: 2,
+        nim: "654321",
+        nama: "Schrievend Timbuleng",
+        nomorTelp: "+62"
       },
     // Tambahkan data anggota lainnya sesuai kebutuhan
   ];
